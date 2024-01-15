83423ffff80d3e5cb4b7d2738a9ece0079d1849e
matthieudrvt <matthieu.drevet69@gmail.com>
2024-01-15 17:12:05 +0100

    Graphique type stat

--- pkmnstats_mean_evolution.py
+++ pkmnstats_mean_evolution.py
@@ -51,3 +51,41 @@ for cl in list_attribute:
 
     # Affichage de l'histogramme
     plt.show()
+    
+def evolution_par_type(type, stat):
+    request2 = f'SELECT "Generation", AVG("{stat}") as avg_stat, COUNT(*) FROM Pokemon WHERE "Type1" = "{type}" OR "Type2" = "{type}" GROUP BY "Generation"' 
+    df_stats_evolution = pd.read_sql(request2, conn)
+    
+    var_X = ['Generation']
+    X = df_stats_evolution[var_X]
+    
+    var_y = 'avg_stat'
+    y = df_stats_evolution[var_y]
+    
+    linreg_model = OLS(y, add_constant(X))
+    linreg = linreg_model.fit()
+    linreg_sk_model = LinearRegression()
+    linreg_sk = linreg_sk_model.fit(X, y)
+    print(linreg.summary())
+
+    a = linreg_sk.coef_
+    cte = linreg_sk.intercept_
+    line_reg = a*X + cte
+
+    hauteurs = y
+    # Numéros pour l'axe des x
+    numeros_x = X['Generation'].tolist()
+    # Création de l'histogramme
+    plt.bar(numeros_x, hauteurs, color='blue', edgecolor='black')
+
+    plt.plot(X, line_reg, "r-")
+    # Ajout de titres et d'étiquettes
+    plt.title(f'Evolution des statistiques des pokémons en fonction de leur génération pour le type {type}')
+    plt.xlabel('Génération')
+    plt.ylabel(f'{stat}')
+
+
+    # Affichage de l'histogramme
+    plt.show()
+
+print(evolution_par_type("Fire", "Attack"))
