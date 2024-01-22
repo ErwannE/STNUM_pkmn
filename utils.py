c05c37a4497db39188d71ea94589f154290cc3da
ErwannE <erwannesteve@gmail.com>
2024-01-22 21:25:40 +0100

    Amelioration de plot_average_attributes

--- utils.py
+++ utils.py
@@ -2,20 +2,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sqlite3
 
-def get_attributes_by_names(list_names):
+def get_attributes_by_names(list_names, attributes=["Attack", "Defense", "Sp. Atk", "Sp. Def", "HP", "Speed", "Total"]): # list_names is a list of strings
     conn = sqlite3.connect('STNUM_pokemon.db')
     names = ', '.join([f'"{name}"' for name in list_names])
-    request = f'SELECT "Attack", "Defense", "Sp. Atk", "Sp. Def", "HP", "Speed", "Total" FROM Pokemon WHERE Name IN ({names})'
+    str_attributes = ', '.join([f'"{attribute}"' for attribute in attributes])
+    request = f'SELECT {str_attributes} FROM Pokemon WHERE Name IN ({names})'
+    print(request)
     result = pd.read_sql(request, conn)
     return result
 
 
-def plot_average_attributes(list_names):
+def plot_average_attributes(list_names, attributes=["Attack", "Defense", "Sp. Atk", "Sp. Def", "HP", "Speed", "Total"]):
     # Assuming you have already connected to the database and retrieved the Pokemon table as a pandas DataFrame
-    pokemon_df = get_attributes_by_names(list_names)
+    pokemon_df = get_attributes_by_names(list_names, attributes)
 
     # Calculate the average of the attributes
-    average_attributes = pokemon_df[["Attack", "Defense", "Sp. Atk", "Sp. Def", "HP", "Speed", "Total"]].mean()
+    average_attributes = pokemon_df[attributes].mean()
 
     # Divide all values in the ""Total" column by 6
     average_attributes["Total"] /= 6
